81f57911cf761a3518d027b9590b2d145f8d1e35
joseph_chang <joseph_chang@davicom.com.tw>
2025-12-19 13:16:43 +0800

    feat:  DHCPv4 客戶端功能 by main(),   (替代原使用 CONFIG_NET_DHCPV4=y, ...等)
    改善日誌輸出

--- samples/net/dhcpv4_client/src/main.c
+++ samples/net/dhcpv4_client/src/main.c
@@ -20,13 +20,13 @@ LOG_MODULE_REGISTER(net_dhcpv4_client_sample, LOG_LEVEL_DBG);
 #include <zephyr/net/net_context.h>
 #include <zephyr/net/net_mgmt.h>
 
-//#define DHCP_OPTION_NTP (42)
+#define DHCP_OPTION_NTP (42)
 
-//static uint8_t ntp_server[4];
+static uint8_t ntp_server[4];
 
-//static struct net_mgmt_event_callback mgmt_cb;
+static struct net_mgmt_event_callback mgmt_cb;
 
-//static struct net_dhcpv4_option_callback dhcp_cb;
+static struct net_dhcpv4_option_callback dhcp_cb;
 
 #define MAIN_BSACIC_COUNT 1000
 int endc = 0;
@@ -35,11 +35,9 @@ static void start_dhcpv4_client(struct net_if *iface, void *user_data)
 {
 	ARG_UNUSED(user_data);
 
-	#if 0
-	// LOG_INF("Start on %s: index=%d", net_if_get_device(iface)->name,
-	//	net_if_get_by_iface(iface));
+	LOG_INF("Start on %s: index=%d", net_if_get_device(iface)->name,
+		net_if_get_by_iface(iface));
 	net_dhcpv4_start(iface);
-	#endif
 }
 
 static void handler(struct net_mgmt_event_callback *cb, uint32_t mgmt_event, struct net_if *iface)
@@ -51,14 +49,12 @@ static void handler(struct net_mgmt_event_callback *cb, uint32_t mgmt_event, str
 	}
 
 	for (i = 0; i < NET_IF_MAX_IPV4_ADDR; i++) {
+		char buf[NET_IPV4_ADDR_LEN];
 
 		if (iface->config.ip.ipv4->unicast[i].ipv4.addr_type != NET_ADDR_DHCP) {
 			continue;
 		}
 
-		#if 0
-		char buf[NET_IPV4_ADDR_LEN];
-		printk("\n(handler.e=%d)\n", endc++); // LOG_INF("%d", endc++);
 		LOG_INF("   Address[%d]: %s", net_if_get_by_iface(iface),
 			net_addr_ntop(AF_INET,
 				      &iface->config.ip.ipv4->unicast[i].ipv4.address.in_addr, buf,
@@ -68,36 +64,33 @@ static void handler(struct net_mgmt_event_callback *cb, uint32_t mgmt_event, str
 				      sizeof(buf)));
 		LOG_INF("    Router[%d]: %s", net_if_get_by_iface(iface),
 			net_addr_ntop(AF_INET, &iface->config.ip.ipv4->gw, buf, sizeof(buf)));
-		// LOG_INF("Lease time[%d]: %u seconds", net_if_get_by_iface(iface),
-		//	iface->config.dhcpv4.lease_time);
-		#endif
+		LOG_INF("Lease time[%d]: %u seconds", net_if_get_by_iface(iface),
+			iface->config.dhcpv4.lease_time);
 	}
 }
 
 static void option_handler(struct net_dhcpv4_option_callback *cb, size_t length,
 			   enum net_dhcpv4_msg_type msg_type, struct net_if *iface)
 {
-	#if 0
 	char buf[NET_IPV4_ADDR_LEN];
 
 	LOG_INF("DHCP Option %d: %s", cb->option,
 		net_addr_ntop(AF_INET, cb->data, buf, sizeof(buf)));
-	#endif
 }
 
 int main(void)
 {
 	LOG_INF("Run dhcpv4 client.s (main.s=%d)", endc++);
 
-//	net_mgmt_init_event_callback(&mgmt_cb, handler, NET_EVENT_IPV4_ADDR_ADD);
-//	net_mgmt_add_event_callback(&mgmt_cb);
+	net_mgmt_init_event_callback(&mgmt_cb, handler, NET_EVENT_IPV4_ADDR_ADD);
+	net_mgmt_add_event_callback(&mgmt_cb);
 
-//	net_dhcpv4_init_option_callback(&dhcp_cb, option_handler, DHCP_OPTION_NTP, ntp_server,
-//					sizeof(ntp_server));
+	net_dhcpv4_init_option_callback(&dhcp_cb, option_handler, DHCP_OPTION_NTP, ntp_server,
+					sizeof(ntp_server));
 
-//	net_dhcpv4_add_option_callback(&dhcp_cb);
+	net_dhcpv4_add_option_callback(&dhcp_cb);
 
-//	net_if_foreach(start_dhcpv4_client, NULL);
+	net_if_foreach(start_dhcpv4_client, NULL);
 
 	LOG_INF("Run dhcpv4 client.e (main.e=%d)", endc++);
 	return 0;
